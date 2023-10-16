9c25e731d32605eb3a672b4b7b12314636bd2cba
Niyilala <106750730+Niyilala@users.noreply.github.com>
2023-10-16 17:06:45 +0100

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,6 +0,0 @@
-# Fix the number of max open files per process
-
-exec {'fix nginx and restart':
-command => "sudo sed -i 's/15/4096/g' /etc/default/nginx; sudo service nginx restart",
-path    => ['/usr/bin', '/bin'],
-}
